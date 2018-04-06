4dc9469fa88f940245c8d835102119442138f862
John Campbell <johncampbell@geospatialweb.ca>
2018-04-06 12:25:33 -0400

    good to go

--- config/webpack_config.js
+++ config/webpack_config.js
@@ -4,9 +4,8 @@
 var resolve = require('path').resolve;
 
 var config = {
-	entry: {
-		app: resolve('./src/index.js')
-	},
+	entry: resolve('./src/index.js'),
+
 	output: {
 		filename: 'app.js',
 		path: resolve('./src/dist')
